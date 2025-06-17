886b5b1e6b665076068cb93b59f613c47773fa6d
faizan <faizanhmd3@gmail.com>
2025-06-17 16:56:09 +0500

    Bottom Navigation Added

--- app/src/main/java/com/example/ecomapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ecomapp/screens/HomeScreen.kt
@@ -6,48 +6,92 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import java.util.Vector
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.ecomapp.pages.CartPage
+import com.example.ecomapp.pages.FavouritePage
+import com.example.ecomapp.pages.HomePage
+import com.example.ecomapp.pages.ProfilePage
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(32.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Home Screen",
-            modifier = Modifier.fillMaxWidth(),
-            style = TextStyle(
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace
-            )
-        )
 
-        Button(onClick = {
-            Firebase.auth.signOut()
-            navController.navigate("auth") {
-                popUpTo("home") {inclusive = true}
+    val navItemList = listOf(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Favourite", Icons.Default.Favorite),
+        NavItem("Cart", Icons.Default.ShoppingCart),
+        NavItem("Profile", Icons.Default.Person)
+    )
+
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                navItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = index == selectedIndex,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                        },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+                }
             }
-        }) {
-            Text(text = "LOGOUT")
         }
+    ) {
+        ContentScreen(modifier = modifier.padding(it), selectedIndex)
     }
 }
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier, selectedIndex: Int) {
+    when (selectedIndex) {
+        0 -> HomePage(modifier)
+        1 -> FavouritePage(modifier)
+        2 -> CartPage(modifier)
+        3 -> ProfilePage(modifier)
+    }
+}
+
+data class NavItem(
+    var label: String,
+    var icon: ImageVector
+)
